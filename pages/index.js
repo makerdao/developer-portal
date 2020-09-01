cbfea2991887efb8feead8471aaf02dadbd71b3c
Phil Bain <bain.pmcg@gmail.com>
2020-09-01 13:49:10 -0400

    json manifest for feat guides

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@
 import { useState } from 'react';
 import { Container, jsx, Card, Heading, Text, Grid, Box, Flex } from 'theme-ui';
 import Link from 'next/link';
+import useResources from 'hooks/useResources';
 import SingleLayout from '../layouts/SingleLayout.js';
+import GuideList from 'components/GuideList';
+import { default as featured } from 'data/featuredGuides.json';
 
 const content = [
   ['decentralized finance', 'text goes here', 'Learn More', '/technology'],
@@ -10,6 +13,11 @@ const content = [
 ];
 
 const Index = () => {
+  const resources = useResources();
+  const guides = resources.filter(r => {
+    const idx = featured.indexOf(r.frontMatter.slug);
+    if (idx !== -1) return r.frontMatter.slug === featured[idx];
+  });
   return (
     <SingleLayout>
       <Container>
@@ -25,7 +33,7 @@ const Index = () => {
             <Text>For developers</Text>
           </Box>
         </Flex>
-
+        <GuideList guides={guides} />
         {content.map(([title, text, cta, ctaRoute]) => {
           return (
             <Flex
