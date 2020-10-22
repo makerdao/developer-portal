e6487d8930da194096d98181655a6bb1949c57ff
Phil Bain <bain.pmcg@gmail.com>
2020-10-22 14:58:03 -0400

    remove unused files

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,5 @@
 export { default as toMarkdownString } from './toMarkdownString';
-export { default as flatDocs } from './flatDocs';
 export { default as createToc } from './createToc.js';
-export { default as theme } from './theme';
-export { default as isActiveNav } from './isNavActive';
 export { default as getRandID } from './getRandID';
-export { default as getBlogPosts } from './getBlogPosts';
 export { default as getResources } from './getResources';
 export { default as validateEmail } from './validateEmail';
