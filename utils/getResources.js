160bc15370a373c400e8de2ce0003e0b46836d1d
Phil Bain <bain.pmcg@gmail.com>
2020-11-17 15:47:06 -0500

    move filter for slug in getResources

--- utils/getResources.js
+++ utils/getResources.js
@@ -9,42 +9,37 @@ const getResources = async (preview, previewData, contentDir) => {
 
   try {
     const resources = await Promise.all(
-      files
-        .filter((file) => {
-          const content = fs.readFileSync(`${file}`, 'utf8');
-          const { data } = matter(content);
-          return data.slug;
-        })
-        .map(async (file) => {
-          if (preview) {
-            const previewProps = await getGithubPreviewProps({
-              ...previewData,
-              fileRelativePath: file,
-              parse: parseMarkdown,
-            });
-            return {
-              fileName: file.substring(contentDir.length + 1, file.length - 3),
-              fileRelativePath: file,
-              data: previewProps.props.file?.data,
-            };
-          }
-          const content = fs.readFileSync(`${file}`, 'utf8');
-          const data = matter(content);
-
+      files.map(async (file) => {
+        if (preview) {
+          const previewProps = await getGithubPreviewProps({
+            ...previewData,
+            fileRelativePath: file,
+            parse: parseMarkdown,
+          });
           return {
             fileName: file.substring(contentDir.length + 1, file.length - 3),
             fileRelativePath: file,
-            data: {
-              frontmatter: data.data,
-              markdownBody: data.content,
-            },
+            data: previewProps.props.file?.data,
           };
-        })
+        }
+        const content = fs.readFileSync(`${file}`, 'utf8');
+        const data = matter(content);
+
+        return {
+          fileName: file.substring(contentDir.length + 1, file.length - 3),
+          fileRelativePath: file,
+          data: {
+            frontmatter: data.data,
+            markdownBody: data.content,
+          },
+        };
+      })
     );
-    return resources;
+    const filtered = resources.filter((file) => file.data.frontmatter.slug);
+    return filtered;
   } catch (e) {
     const source = preview ? 'Github' : 'filesystem';
-    throw new Error(`Error fetching resources from ${source}: ${e}`);
+    throw new Error(`Error fetching files from ${source}: ${e}`);
   }
 };
 
