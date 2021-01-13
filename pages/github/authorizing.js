4b4b4b986d27434f1560eace7699bda475f996ef
Phil Bain <bain.pmcg@gmail.com>
2021-01-13 12:09:51 -0700

    add theme-ui to authorizing page

--- pages/github/authorizing.js
+++ pages/github/authorizing.js
@@ -1,9 +1,11 @@
-import { useGithubAuthRedirect } from "react-tinacms-github"
+/** @jsx jsx */
+import { jsx, Container } from 'theme-ui';
+import { useGithubAuthRedirect } from 'react-tinacms-github';
 
 // Our GitHub app redirects back to this page with auth code
 export default function Authorizing() {
   // Let the main app know, that we received an auth code from the GitHub redirect
-  useGithubAuthRedirect()
+  useGithubAuthRedirect();
 
-  return <h2>Authorizing with GitHub, please wait...</h2>
+  return <Container>Authorizing with GitHub, please wait...</Container>;
 }
