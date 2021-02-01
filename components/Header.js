fc11df59891a4a3d60097d67cd7cc3948a189eac
Phil Bain <bain.pmcg@gmail.com>
2021-02-01 14:38:02 -0700

    clean up header

--- components/Header.js
+++ components/Header.js
@@ -35,80 +35,29 @@ const ColorModeToggle = (props) => {
 };
 
 const LINKS = [
-  { url: '/technology', name: 'Technology' },
   { url: '/documentation', name: 'Documentation' },
   { url: '/guides', name: 'Guides' },
   { url: '/security', name: 'Security' },
 ];
 
-const NavLinks = ({ mobileOpened, setMobileOpened, setPopupState, query }) =>
-  LINKS.map(({ name, url, type, popup }) => (
-    <Fragment key={name}>
-      {type === 'menu' ? (
-        mobileOpened ? null : (
-          <Text
-            sx={{ pr: 4, '&:last-child': { pr: [null, 0] } }}
-            key={name}
-            variant="links.nav"
-            onMouseEnter={
-              popup &&
-              ((e) => {
-                const targetRect = e.target.getBoundingClientRect();
-                setPopupState({
-                  name,
-                  show: true,
-                  left: targetRect.left - targetRect.width / 2,
-                  top: targetRect.bottom,
-                });
-              })
-            }
-          >
-            {name}
-          </Text>
-        )
-      ) : (
-        <Link href={{ pathname: url, query }} passHref key={name}>
-          {mobileOpened ? (
-            <NavLink
-              sx={{ '&:last-child': { pr: [null, 0] } }}
-              onClick={() => setMobileOpened(false)}
-              variant="links.nav"
-            >
-              {name}
-            </NavLink>
-          ) : (
-            <NavLink
-              key={name}
-              sx={{
-                pr: 4,
-                '&:last-child': { pr: [null, 0] },
-              }}
-              onMouseEnter={
-                popup &&
-                ((e) => {
-                  const targetRect = e.target.getBoundingClientRect();
-                  setPopupState({
-                    name,
-                    show: true,
-                    left: targetRect.left - targetRect.width / 2,
-                    top: targetRect.bottom,
-                  });
-                })
-              }
-              variant="links.nav"
-            >
-              {name}
-            </NavLink>
-          )}
-        </Link>
-      )}
-    </Fragment>
+const NavLinks = ({ query }) =>
+  LINKS.map(({ name, url }) => (
+    <Link href={{ pathname: url, query }} passHref key={name}>
+      <NavLink
+        key={name}
+        sx={{
+          pr: 4,
+          '&:last-child': { pr: [null, 0] },
+        }}
+        variant="links.nav"
+      >
+        {name}
+      </NavLink>
+    </Link>
   ));
 
 const Header = ({ query, subnav }) => {
   const [mobileOpened, setMobileOpened] = useState(false);
-  const [popupState, setPopupState] = useState({ show: false });
-  const { show, left, top, name } = popupState;
   return (
     <>
       <Container
@@ -133,23 +82,9 @@ const Header = ({ query, subnav }) => {
               as="nav"
               sx={{
                 alignItems: 'center',
-                // display: [mobileOpened ? 'flex' : 'none', 'flex'],
-                // ...(mobileOpened && {
-                //   position: ['fixed', 'initial'],
-                //   top: ['0', 'initial'],
-                //   bottom: ['0', 'initial'],
-                //   left: ['0', 'initial'],
-                //   right: ['0', 'initial'],
-                //   bg: ['surface', 'initial'],
-                //   alignItems: ['center', 'initial'],
-                //   justifyContent: ['flex-start', 'initial'],
-                //   flexDirection: ['column', 'initial'],
-                //   pt: [6, 'initial'],
-                // }),
               }}
             >
-              <NavLinks {...{ mobileOpened, setMobileOpened, setPopupState, query }} />
-              {/* <MenuPopup setState={setPopupState} show={show} left={left} top={top} name={name} /> */}
+              <NavLinks {...{ mobileOpened, setMobileOpened, query }} />
               <ColorModeToggle />
             </Flex>
             <Icon
