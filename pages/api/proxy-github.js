ef183ad2b007b0fea764c0fea2600a65855f7282
Phil Bain <bain.pmcg@gmail.com>
2021-03-02 14:54:37 -0700

    handle tina errors better

--- pages/api/proxy-github.js
+++ pages/api/proxy-github.js
@@ -1,3 +1,16 @@
 import { apiProxy } from 'next-tinacms-github';
 
-export default apiProxy(process.env.SIGNING_KEY);
+export default async (req, res) => {
+  try {
+    const result = await apiProxy(process.env.SIGNING_KEY)(req, res);
+    return result;
+  } catch (e) {
+    console.error('Error with github apiProxy', e);
+    return res.status(500).json({
+      error: {
+        code: 'unexpected_error',
+        message: 'An unexpected error occurred with the github apiProxy.',
+      },
+    });
+  }
+};
